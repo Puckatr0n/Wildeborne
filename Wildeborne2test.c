6442386d5c69e7779ee9c4e8250958c8a6e66eb3
Puckatr0n <adampuckey@gmail.com>
2022-09-13 16:50:57 +0100

    message3

---
+++ Wildeborne2test.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+welcomeMessage(){
+	printf("Welcome to the wonderful world of Wildeborne.\n");
+}
+int main(){
+	welcomeMessage():
+	printf("HEllow and some ovvur fings")
+	printf("aint got nuffin")
+	return 0;
+}
