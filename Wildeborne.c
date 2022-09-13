8523e14fc9b108b24339f9daab3ca65dc38f6755
Puckatr0n <adampuckey@gmail.com>
2022-09-13 19:34:42 +0100

    Added the beginnings of the village index, and expanded the welcome message.

--- Wildeborne.c
+++ Wildeborne.c
@@ -1,10 +1,32 @@
 #include<stdio.h>
-welcomeMessage(){
-	printf("Welcome to the wonderful world of Wildeborne.\n");
+void welcomeMessage(){
+	printf("Welcome to the wonderful village of Wildeborne.\n");
+	printf("After travelling long and far after your last daring adventure,\n");
+	printf("you have made your way here for some much needed relaxation and\n");
+	printf("downtime.\n");
+	printf("Wildeborne has a few interesting locations for the lone adventurer\n");
+	printf("to explore.\n");
+}
+void villageIndex(){
+printf("Where would you like to go?\n1. The pub.\n2. The stables.\nPlease select an option and press enter: ");
+	char villageOption;
+	scanf("%c", &villageOption);
+	if (villageOption == '1')
+	{
+		printf("pub placeholder text\n");
+		villageIndex();
+	} else if (villageOption == '2') 
+	{
+		printf("Stables placeholder text\n");
+		villageIndex();
+	} else 
+	{
+		printf("That wasn't an option!\n");
+		villageIndex();
+	}
 }
 int main(){
-	welcomeMessage():
-	printf("HEllow and some ovvur fings")
-	printf("bippety bippety bop")
+	welcomeMessage();
+	villageIndex();
 	return 0;
 }
